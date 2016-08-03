a59a56215f762f7344f2d1d24698867715b9b261
Volker Birk <vb@pep.foundation>
2016-08-03 14:10:22 +0200

    ...

--- src/pEpmodule.hh
+++ src/pEpmodule.hh
@@ -5,6 +5,7 @@
 namespace pEp {
     namespace PythonAdapter {
         using namespace std;
+
         const char *version_string = "p≡p Python adapter version 0.1";
         string about();
     }
