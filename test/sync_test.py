1b8cabd299d5c170a31b730efcef96996e501733
Volker Birk <vb@pep.foundation>
2016-08-23 18:57:58 +0200

    sync_test.py

---
+++ test/sync_test.py
@@ -0,0 +1,21 @@
+from pEp import *
+
+_msg = []
+_me = None
+_partner = None
+
+class Handler(SyncMixIn):
+    def messageToSend(self, msg):
+        global _msg
+        print("-" * 80)
+        print(str(msg).replace("\r", ""))
+        _msg.append(msg)
+
+    def showHandshake(self, me, partner):
+        print("handshake needed between " + repr(me) + " and " + repr(partner))
+        global _me, _partner
+        _me, _partner = (me, partner)
+
+
+handler = Handler()
+
