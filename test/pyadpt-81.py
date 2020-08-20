74dcc6f7a7e9682ecb0ee14c5ef3146829e08373
heck <heck@pep.foundation>
2020-08-20 14:36:37 +0200

    test pyadpt-81.py: test start/stop sync, default/user callback impl. of pEp.message_to_send()

--- test/pyadpt-81.py
+++ test/pyadpt-81.py
@@ -2,6 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import pEp
+import time
+
+def user_message_to_send(msg):
+    print("User defined callback implementation")
+
+def start_stop_sync(duration):
+    pEp.start_sync()
+    time.sleep(duration)
+    pEp.shutdown_sync()
 
 dir(pEp)
-pEp.is_sync_active()
+# test default callback
+start_stop_sync(1)
+# test user defined callback
+pEp.message_to_send = user_message_to_send
+start_stop_sync(1)
+
+pEp.start_sync()
+while(True):
+    print("is_sync_active: {}".format(pEp.is_sync_active()))
+    time.sleep(1)
