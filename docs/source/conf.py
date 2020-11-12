04cd048ee41ac17dda468f6f04a738531f0b69df
heck <heck@hecks-MacBook-Pro.local>
2020-11-12 18:05:00 +0100

    Update author credits

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('../../src'))
 # -- Project information -----------------------------------------------------
 
 project = "pEpPythonAdapter"
-copyright = "2020, Volker Birk, heck, juga"
-author = "Volker Birk, heck, juga"
+copyright = "2020, Volker Birk, heck, Hartmut Goebel, juga"
+author = "Volker Birk, heck, Hartmut Goebel, juga"
 
 import pEp
 # The full version, including alpha/beta/rc tags
