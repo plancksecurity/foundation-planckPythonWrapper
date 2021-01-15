dc0b76913652af7c94b6b54207c7283b3ecd758b
heck <heck@pep.foundation>
2021-01-15 03:07:59 +0100

    _gen Add PEP_SESSION

--- src/pEp/_gen/pEpModule.cc
+++ src/pEp/_gen/pEpModule.cc
@@ -5,14 +5,23 @@
 #include "adapter_main.hh"
 
 using namespace std;
+namespace alib = pEp::Adapter;
 
 string testfunc() {
     return "fsdfg";
 }
 
+void *getSessionHandle() {
+    void *handle = static_cast<void *>(alib::session());
+    return handle;
+}
+
 PYBIND11_MODULE(_gen, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
     m.def("add", &testfunc, "A function which adds two numbers");
+
+    // PEP_SESSION
+    m.def("get_handle", &getSessionHandle);
 
     #include "gen/py_module.pybind11"
 
