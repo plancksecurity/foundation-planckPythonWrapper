f5d83b7454d48599f6549f70b0e5f9791e35316f
Hartmut Goebel <h.goebel@crazy-compilers.com>
2021-06-24 11:16:29 +0200

    PYADPT-116: Clean up test-suite.

--- tests/test_blob.py
+++ tests/test_blob.py
@@ -4,18 +4,15 @@
 
 """Blob unit tests."""
 
-import pytest
 
-from . import constants
-from . import model
-
-def test_blob_data_constructor(pEp, model):
+def test_blob_data_constructor(pEp):
     bdata = b'this is binary \x00\x01\xbb\xa7\xa4\xab test data'
     b = pEp.Blob(bdata)
-    assert(b.data == bdata)
+    assert b.data == bdata
 
-def test_blob_data_property(pEp, model):
+
+def test_blob_data_property(pEp):
     bdata = b'this is binary \x00\x01\xbb\xa7\xa4\xab test data'
     b = pEp.Blob(b'dummy')
     b.data = bdata
-    assert(b.data == bdata)
+    assert b.data == bdata
