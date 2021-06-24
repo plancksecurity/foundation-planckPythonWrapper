312c6d9b92c46a21896eb35e8d4bdc1c980d475a
Hartmut Goebel <h.goebel@crazy-compilers.com>
2021-06-24 11:08:41 +0200

    PYADPT-116: Make tests use binary data, esp. null-bytes.

--- tests/test_blob.py
+++ tests/test_blob.py
@@ -10,12 +10,12 @@ from . import constants
 from . import model
 
 def test_blob_data_constructor(pEp, model):
-    bdata = bytes('this is test data', 'utf-8')
+    bdata = b'this is binary \x00\x01\xbb\xa7\xa4\xab test data'
     b = pEp.Blob(bdata)
     assert(b.data == bdata)
 
 def test_blob_data_property(pEp, model):
+    bdata = b'this is binary \x00\x01\xbb\xa7\xa4\xab test data'
     b = pEp.Blob(b'dummy')
-    bdata = bytes('this is test data', 'utf-8')
     b.data = bdata
     assert(b.data == bdata)
